bd5379394839ac195feb19127f7c9074fccf1286
Arian Samiei <ariansamiei@gmail.com>
2024-11-06 11:26:36 +0330

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ export const apiUrl = "https://chatbot.avayes.com/api";
 
 export const global_header = {
   Accept: "application/json",
-  // Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzEwOTI2NDg3LCJpYXQiOjE3MTAzMjE2ODcsImp0aSI6IjEzYmQ2YjYxMGY1YjQ0ZTFiNzVlYWIyODlkNmJlOWZkIiwidXNlcl9pZCI6M30.e54KiZICZ8k-_5pl9LjU_v03-RZWzO-qCpUbOV0RujQ`,
 };
 
 // export const API_DOWNLOAD = (id) => `${apiUrl}/camera/download/?camera_id=${id}`;
